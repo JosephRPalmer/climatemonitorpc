a465f3a33e154d542ace952e124c0bea6541cc01
Joseph Palmer <joseph@ryan-palmer.com>
2013-04-03 12:11:43 +0100

    Added UI to graph

--- a2mokup/graph.Designer.vb
+++ a2mokup/graph.Designer.vb
@@ -22,16 +22,28 @@ Partial Class graph
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.closegraph = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'closegraph
+        '
+        Me.closegraph.Location = New System.Drawing.Point(507, 394)
+        Me.closegraph.Name = "closegraph"
+        Me.closegraph.Size = New System.Drawing.Size(106, 23)
+        Me.closegraph.TabIndex = 0
+        Me.closegraph.Text = "Close Graphs"
+        Me.closegraph.UseVisualStyleBackColor = True
         '
         'graph
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(625, 429)
+        Me.Controls.Add(Me.closegraph)
         Me.Name = "graph"
         Me.Text = "Graphs"
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents closegraph As System.Windows.Forms.Button
 End Class
