d0b4be3495f5a5acdc7d17f7af9130d668e14c0f
Joseph Palmer <joseph@ryan-palmer.com>
2013-04-03 12:51:19 +0100

    Connected to SQL Database

--- a2mokup/graph.Designer.vb
+++ a2mokup/graph.Designer.vb
@@ -23,6 +23,8 @@ Partial Class graph
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.closegraph = New System.Windows.Forms.Button()
+        Me.graphingbox = New System.Windows.Forms.PictureBox()
+        CType(Me.graphingbox, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'closegraph
@@ -34,16 +36,27 @@ Partial Class graph
         Me.closegraph.Text = "Close Graphs"
         Me.closegraph.UseVisualStyleBackColor = True
         '
+        'graphingbox
+        '
+        Me.graphingbox.Location = New System.Drawing.Point(12, 12)
+        Me.graphingbox.Name = "graphingbox"
+        Me.graphingbox.Size = New System.Drawing.Size(601, 376)
+        Me.graphingbox.TabIndex = 1
+        Me.graphingbox.TabStop = False
+        '
         'graph
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(625, 429)
+        Me.Controls.Add(Me.graphingbox)
         Me.Controls.Add(Me.closegraph)
         Me.Name = "graph"
         Me.Text = "Graphs"
+        CType(Me.graphingbox, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents closegraph As System.Windows.Forms.Button
+    Friend WithEvents graphingbox As System.Windows.Forms.PictureBox
 End Class
--- a2mokup/graph.vb
+++ a2mokup/graph.vb
@@ -1,6 +1,12 @@
-﻿Public Class graph
+﻿Imports System.Drawing
+Imports System.Threading
+Public Class graph
 
     Private Sub closegraph_Click(sender As Object, e As EventArgs) Handles closegraph.Click
         Me.Hide()
+    End Sub
+
+    Private Sub graph_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
     End Sub
 End Class
