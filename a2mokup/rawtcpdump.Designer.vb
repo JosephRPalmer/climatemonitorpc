e967caadef02e3d670b00eab169a09e8837fccf9
Joseph Palmer <joseph@ryan-palmer.com>
2013-04-04 10:38:21 +0100

    Added close,clear,save to log
    save requires elevated priveleges which are not yet functional

--- a2mokup/rawtcpdump.Designer.vb
+++ a2mokup/rawtcpdump.Designer.vb
@@ -23,21 +23,54 @@ Partial Class rawtcpdump
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.tcpdump = New System.Windows.Forms.RichTextBox()
+        Me.savetcpdump = New System.Windows.Forms.Button()
+        Me.cleartcpdump = New System.Windows.Forms.Button()
+        Me.closetcpdump = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'tcpdump
         '
         Me.tcpdump.Location = New System.Drawing.Point(12, 12)
         Me.tcpdump.Name = "tcpdump"
-        Me.tcpdump.Size = New System.Drawing.Size(453, 288)
+        Me.tcpdump.Size = New System.Drawing.Size(453, 269)
         Me.tcpdump.TabIndex = 0
         Me.tcpdump.Text = ""
+        '
+        'savetcpdump
+        '
+        Me.savetcpdump.Location = New System.Drawing.Point(12, 287)
+        Me.savetcpdump.Name = "savetcpdump"
+        Me.savetcpdump.Size = New System.Drawing.Size(144, 23)
+        Me.savetcpdump.TabIndex = 1
+        Me.savetcpdump.Text = "Save TCP Dump"
+        Me.savetcpdump.UseVisualStyleBackColor = True
+        '
+        'cleartcpdump
+        '
+        Me.cleartcpdump.Location = New System.Drawing.Point(162, 287)
+        Me.cleartcpdump.Name = "cleartcpdump"
+        Me.cleartcpdump.Size = New System.Drawing.Size(151, 23)
+        Me.cleartcpdump.TabIndex = 2
+        Me.cleartcpdump.Text = "Clear TCP Dump"
+        Me.cleartcpdump.UseVisualStyleBackColor = True
+        '
+        'closetcpdump
+        '
+        Me.closetcpdump.Location = New System.Drawing.Point(319, 287)
+        Me.closetcpdump.Name = "closetcpdump"
+        Me.closetcpdump.Size = New System.Drawing.Size(146, 23)
+        Me.closetcpdump.TabIndex = 3
+        Me.closetcpdump.Text = "Close "
+        Me.closetcpdump.UseVisualStyleBackColor = True
         '
         'rawtcpdump
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(477, 312)
+        Me.ClientSize = New System.Drawing.Size(477, 315)
+        Me.Controls.Add(Me.closetcpdump)
+        Me.Controls.Add(Me.cleartcpdump)
+        Me.Controls.Add(Me.savetcpdump)
         Me.Controls.Add(Me.tcpdump)
         Me.Name = "rawtcpdump"
         Me.Text = "Raw TCP Dump"
@@ -45,4 +78,7 @@ Partial Class rawtcpdump
 
     End Sub
     Friend WithEvents tcpdump As System.Windows.Forms.RichTextBox
+    Friend WithEvents savetcpdump As System.Windows.Forms.Button
+    Friend WithEvents cleartcpdump As System.Windows.Forms.Button
+    Friend WithEvents closetcpdump As System.Windows.Forms.Button
 End Class
--- a2mokup/rawtcpdump.vb
+++ a2mokup/rawtcpdump.vb
@@ -11,4 +11,29 @@
         rawtcpdump.tcpdump.Text = rawtcpdump.tcpdump.Text + timestamp + write + vbCrLf
         Return 1
     End Function
+    Private Sub texchangetcpdump() Handles tcpdump.TextChanged
+        mainform.scroltocaret(Me.tcpdump)
+    End Sub
+
+    Private Sub savetcpdump_Click(sender As Object, e As EventArgs) Handles savetcpdump.Click
+        Dim time As DateTime = DateTime.Now
+        Dim format As String = "dddd.MMM.yyyy.HH.mm.ss"
+        Dim timestamp As String
+        timestamp = (time.ToString(format))
+        tcpdump.SaveFile("C:\tcpdump" + timestamp + ".txt", RichTextBoxStreamType.PlainText)
+    End Sub
+
+    Private Sub cleartcpdump_Click(sender As Object, e As EventArgs) Handles cleartcpdump.Click
+
+        If MsgBox("Do you really want to clear the TCP Dump?", MsgBoxStyle.YesNo, "Preparing to clear the TCP Dump...") = MsgBoxResult.Yes Then
+            tcpdump.Clear()
+        Else
+            Exit Sub
+
+        End If
+    End Sub
+
+    Private Sub closetcpdump_Click(sender As Object, e As EventArgs) Handles closetcpdump.Click
+        Me.Hide()
+    End Sub
 End Class
