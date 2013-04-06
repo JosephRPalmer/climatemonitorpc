57aeb8264ba41c3dc571480bca4258b2587bb3a7
Joseph Palmer <joseph@ryan-palmer.com>
2013-04-06 15:39:41 +0100

    Parse XML
    Parse and find specifics in XML feed using proprietary function

--- a2mokup/weatherapi.vb
+++ a2mokup/weatherapi.vb
@@ -1,277 +1,50 @@
-﻿'Module weatherapi
-'    Public Class Features
-
-'        Public Property forecast As Integer
-'        End Property
-'    End Class
-
-'    Public Class Response
-
-'        Public Property version As String
-'        End Property
-
-'        Public Property termsofService As String
-'        End Property
-
-'        Public Property features As Features
-'        End Property
-'    End Class
-'    Public Class Forecastday
-
-'        Public Property period As Integer
-'        End Property
-
-'        Public Property icon As String
-'        End Property
-
-'        Public Property icon_url As String
-'        End Property
-
-'        Public Property title As String
-'        End Property
-
-'        Public Property fcttext As String
-'        End Property
-
-'        Public Property fcttext_metric As String
-'        End Property
-
-'        Public Property pop As String
-'        End Property
-'    End Class
-'    Public Class TxtForecast
-
-'     Public Property date As String
-'        End Property
-
-'        Public Property forecastday As List(Of Forecastday)
-'        End Property
-'    End Class
-' Public Class Date
-
-'        Public Property epoch As String
-'        End Property
-
-'        Public Property pretty As String
-'        End Property
-
-'        Public Property day As Integer
-'        End Property
-
-'        Public Property month As Integer
-'        End Property
-
-'        Public Property year As Integer
-'        End Property
-
-'        Public Property yday As Integer
-'        End Property
-
-'        Public Property hour As Integer
-'        End Property
-
-'        Public Property min As String
-'        End Property
-
-'        Public Property sec As Integer
-'        End Property
-
-'        Public Property isdst As String
-'        End Property
-
-'        Public Property monthname As String
-'        End Property
-
-'        Public Property weekday_short As String
-'        End Property
-
-'        Public Property weekday As String
-'        End Property
-
-'        Public Property ampm As String
-'        End Property
-
-'        Public Property tz_short As String
-'        End Property
-
-'        Public Property tz_long As String
-'        End Property
-'    End Class
-'    Public Class High
-
-'        Public Property fahrenheit As String
-'        End Property
-
-'        Public Property celsius As String
-'        End Property
-'    End Class
-'    Public Class Low
-
-'        Public Property fahrenheit As String
-'        End Property
-
-'        Public Property celsius As String
-'        End Property
-'    End Class
-'    Public Class QpfAllday
-
-'     Public Property in As Double
-'        End Property
-
-'        Public Property mm As Double
-'        End Property
-'    End Class
-'    Public Class QpfDay
-
-'     Public Property in As Double
-'        End Property
-
-'        Public Property mm As Double
-'        End Property
-'    End Class
-'    Public Class QpfNight
-
-'     Public Property in As Double
-'        End Property
-
-'        Public Property mm As Double
-'        End Property
-'    End Class
-'    Public Class SnowAllday
-
-'     Public Property in As Integer
-'        End Property
-
-'        Public Property cm As Integer
-'        End Property
-'    End Class
-'    Public Class SnowDay
-
-'     Public Property in As Integer
-'        End Property
-
-'        Public Property cm As Integer
-'        End Property
-'    End Class
-'    Public Class SnowNight
-
-'     Public Property in As Integer
-'        End Property
-
-'        Public Property cm As Integer
-'        End Property
-'    End Class
-'    Public Class Maxwind
-
-'        Public Property mph As Integer
-'        End Property
-
-'        Public Property kph As Integer
-'        End Property
-
-'        Public Property dir As String
-'        End Property
-
-'        Public Property degrees As Integer
-'        End Property
-'    End Class
-'    Public Class Avewind
-
-'        Public Property mph As Integer
-'        End Property
-
-'        Public Property kph As Integer
-'        End Property
-
-'        Public Property dir As String
-'        End Property
-
-'        Public Property degrees As Integer
-'        End Property
-'    End Class
-'    Public Class Forecastday2
-
-'     Public Property date As Date
-'        End Property
-
-'        Public Property period As Integer
-'        End Property
-
-'        Public Property high As High
-'        End Property
-
-'        Public Property low As Low
-'        End Property
-
-'        Public Property conditions As String
-'        End Property
-
-'        Public Property icon As String
-'        End Property
-
-'        Public Property icon_url As String
-'        End Property
-
-'        Public Property skyicon As String
-'        End Property
-
-'        Public Property pop As Integer
-'        End Property
-
-'        Public Property qpf_allday As QpfAllday
-'        End Property
-
-'        Public Property qpf_day As QpfDay
-'        End Property
-
-'        Public Property qpf_night As QpfNight
-'        End Property
-
-'        Public Property snow_allday As SnowAllday
-'        End Property
-
-'        Public Property snow_day As SnowDay
-'        End Property
-
-'        Public Property snow_night As SnowNight
-'        End Property
-
-'        Public Property maxwind As Maxwind
-'        End Property
-
-'        Public Property avewind As Avewind
-'        End Property
-
-'        Public Property avehumidity As Integer
-'        End Property
-
-'        Public Property maxhumidity As Integer
-'        End Property
-
-'        Public Property minhumidity As Integer
-'        End Property
-'    End Class
-'    Public Class Simpleforecast
-
-'        Public Property forecastday As List(Of Forecastday2)
-'        End Property
-'    End Class
-'    Public Class Forecast
-
-'        Public Property txt_forecast As TxtForecast
-'        End Property
-
-'        Public Property simpleforecast As Simpleforecast
-'        End Property
-'    End Class
-'    Public Class RootObject
-
-'        Public Property response As Response
-'        End Property
-
-'        Public Property forecast As Forecast
-'        End Property
-'    End Class
-
-
-'End Module
+﻿Imports System.Xml
+Imports System.IO
+Imports System.Text
+Imports System.Net
+Public Class WeatherAPI
+    Friend xmlstring As String = " "
+    Sub writeapi()
+        Dim windspeed As String = ""
+        Dim postfirst As String = "PR9"
+        Dim inStream As StreamReader
+        Dim webRequest As WebRequest
+        Dim webresponse As WebResponse
+        webRequest = webRequest.Create("http://www.myweather2.com/developer/forecast.ashx?uac=KTRvlm3BTu&output=xml&query=" + postfirst)
+        webresponse = webRequest.GetResponse()
+        inStream = New StreamReader(webresponse.GetResponseStream())
+        xmlstring = inStream.ReadToEnd.ToString()
+        My.Computer.FileSystem.WriteAllText("C:\Users\Joseph\Test.txt", xmlstring, True)
+        ''concatonates temperature correctly
+        'Dim currenttemperature As String = ""
+        'If wea.xmlstring.Substring(70, 7) = "</temp>" Then
+        '    currenttemperature = wea.xmlstring.Substring(69, 1)
+        'ElseIf wea.xmlstring.Substring(71, 7) = "</temp>" Then
+        '    currenttemperature = wea.xmlstring.Substring(69, 2)
+        'ElseIf wea.xmlstring.Substring(72, 7) = "</temp>" Then
+        '    currenttemperature = wea.xmlstring.Substring(69, 3)
+        'Else
+        '    currenttemperature = "--"
+        'End If
+        'currenttemperature = currenttemperature + " 'c"
+        ''concatonates windspeed correctly
+        'windspeed = wea.getvalue("<speed>", "</speed>")
+        'windspeed = windspeed + " KPH"
+
+        'mainformfunctions.infoinlog("Temperature in " + postfirst + " = " + currenttemperature)
+    End Sub
+    Function getvalue(initialtag As String, finaltag As String) As String
+        Dim temp As String = xmlstring
+        Do While temp.Substring(0, 7) <> initialtag
+            temp.Remove(0, 1)
+        Loop
+        temp.Remove(0, 7)
+        Dim i As Integer
+        Do While temp.Substring(i, finaltag.Length) <> finaltag
+            i += 1
+        Loop
+        getvalue = temp.Substring(0, i - 1)
+        Return getvalue
+    End Function
+
+End Class
